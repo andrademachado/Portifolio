25a088fc0cfa6fe9ca278ee8181765b63b1c7d77
Lígia Andrade <andradeligiamp@gmail.com>
2024-07-19 18:06:01 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Teste from './teste'
+import EstiloGlobal from './styles'
+
+function App() {
+  return (
+    <>
+      <EstiloGlobal />
+      <Teste />
+    </> 
+  )
+}
+
+export default App
